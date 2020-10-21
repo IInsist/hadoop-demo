a176e66188c076ebebda89921df1cca8c569ada8
Mr.Fu <896930409@qq.com>
2020-10-21 11:16:25 +0800

    相关代码提交

--- src/main/java/com/hadoop/demo/hdfs/service/impl/HdfsServiceImpl.java
+++ src/main/java/com/hadoop/demo/hdfs/service/impl/HdfsServiceImpl.java
@@ -1,21 +1,75 @@
 package com.hadoop.demo.hdfs.service.impl;
 
 import com.hadoop.demo.hdfs.service.HdfsService;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author fujh
+ * @since 2020年10月21日10:52:46
+ * 操作HDFS业务实现类：
+ *  实现操作HDFS核心的两个类：
+ *      1、org.apache.hadoop.conf.Configuration  ==》 配置类
+ *      2、org.apache.hadoop.fs.FileSystem  ==》 操作类
+ */
 @Service
 public class HdfsServiceImpl implements HdfsService {
 
+    /**
+     * HDFS地址
+     */
     @Value("${hdfs.path}")
-    private String path;
+    private static String path;
 
+    /**
+     * HDFS操作用户
+     */
     @Value("${hdfs.user}")
-    private String user;
+    private static String user;
+
+    /**
+     * 配置HDFS配置类
+     * @return
+     */
+    private static Configuration getConfiguration(){
+        Configuration configuration = new Configuration();
+        /**
+         * 配置HDFS地址
+         */
+        configuration.set("fs.defaultFS",path);
+        /**
+         * 临时存储目录（配置的还是服务器上的地址）
+         */
+        configuration.set("hadoop.tmp.dir","/root/software/hadoop-2.6.0/tmp");
+        /**
+         * 副本系数为1
+         */
+        configuration.set("dfs.replication","1");
+        return configuration;
+    }
+
+    /**
+     * 配置HDFS文件系统类FileSystem
+     * @return
+     * @throws URISyntaxException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private static FileSystem getFileSystem() throws URISyntaxException, IOException, InterruptedException {
+        /**
+         * 配置HDFS地址，配置类，操作用户
+         */
+        return FileSystem.get(new URI(path),getConfiguration(),user);
+    }
 
     @Override
     public String showInfo() {
         return path+"---"+user;
     }
-
 }
---
+++ src/test/java/com/hadoop/demo/hdfs/HdfsTest.java
@@ -0,0 +1,24 @@
+package com.hadoop.demo.hdfs;
+
+import com.hadoop.demo.hdfs.service.HdfsService;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author
+ * @since 2020年10月21日11:11:40
+ * HDFS测试类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HdfsTest {
+
+    @Resource
+    private HdfsService service;
+
+
+
+}
